88bf1d9bf8e315ca42cad029ad42e00254326993
shemul hossain <shemulworld@gmail.com>
2015-04-18 12:41:50 +0600

    ADDED CHAPTER 6 FIRST EXERCISE FROM BOOK

--- chpt-6/1.asm
+++ chpt-6/1.asm
@@ -1,4 +1,5 @@
-; Chapter 6 , Ex: 8 from Book
+; Chapter 6 , Ex: 8 from Book   
+; READ TWO CAPTITAL CHAR AND NEXT LINE DISPLAY ALPHABETICAL ORDER
 
 .MODEL SMALL
 
@@ -8,8 +9,45 @@
 
 .CODE
     MAIN PROC 
+      
+      MOV AH , 2
+      MOV DL , '?'
+      INT 21H  
         
-        
+      ;INPUT
+      MOV AH , 1 
+      INT 21H
+      MOV BL , AL 
+      
+      INT 21H  
+      MOV CL , AL
+      
+      
+      ;COMPARE
+      CMP BL , CL 
+      JG SWITCH
+      JMP DISPLAY 
+    
+ SWITCH: XCHG BL , CL   
+         
+ 
+ DISPLAY: 
+      MOV AH , 2  
+      
+      MOV DL , 0AH
+      INT 21H     
+      
+      MOV DL , BL 
+      INT 21H
+      
+      MOV DL , CL
+      INT 21H 
+           
+             
+      ;TERMINATION            
+      MOV AH , 4CH   
+      INT 21H
+      
         
     MAIN ENDP
 END MAIN
